cc347556d953b2c9cbec05fb6a93bdf3f43e792e
chiyongs <tlscldyd@gmail.com>
2020-12-26 03:42:00 +0900

    Style clock and name_input

--- greeting.js
+++ greeting.js
@@ -1,23 +1,54 @@
 // const form = document.querySelector(".js-form"),
 //   input = form.querySelector("input"),
 //   greeting = document.querySelector(".js-greetings");
-const name = document.querySelector(".js-name");
+const nameInput = document.querySelector(".js-name");
 const USER_LS = "currentUser";
 
-function loadName(){
-  const currentUser = localStorage.getItem(USER_LS);
-  if(currentUser == null){
-    
-    }
-  }
+function printInput(value) {
+  nameInput.innerHTML = "";
+  const title = document.createElement("span");
+  title.className = "name__title";
+  title.innerText = `Hello ${value}`;
+  console.log(value);
+  nameInput.appendChild(title);
+}
 
-function init(){
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const input = form.querySelector("input");
+  const value = input.value;
+  localStorage.setItem(USER_LS, value);
+  printInput(value);
+}
+
+function makeInput() {
+  const input = document.createElement("input");
+  input.placeholder = "What is your name?";
+  input.type = "text";
+  input.className = "name__input";
+  const nameForm = document.createElement("form");
+  nameForm.addEventListener("submit", handleSubmit);
+  nameForm.appendChild(input);
+  nameInput.appendChild(nameForm);
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser == null) {
+    makeInput();
+  } else {
+    printInput(currentUser);
+  }
+}
+
+function init() {
   loadName();
 }
 
 init();
 // const USER_LS = "currentUser",
-  // SHOWING_CN = "showing";
+// SHOWING_CN = "showing";
 
 // function saveName(text) {
 //   localStorage.setItem(USER_LS, text);
