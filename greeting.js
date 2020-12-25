2743e34218d1a6081420235e39b5ae43f0cd114a
chiyongs <tlscldyd@gmail.com>
2020-12-26 01:58:41 +0900

    Add some css

--- greeting.js
+++ greeting.js
@@ -1,43 +1,57 @@
-const form = document.querySelector(".js-form"),
-  input = form.querySelector("input"),
-  greeting = document.querySelector(".js-greetings");
+// const form = document.querySelector(".js-form"),
+//   input = form.querySelector("input"),
+//   greeting = document.querySelector(".js-greetings");
+const name = document.querySelector(".js-name");
+const USER_LS = "currentUser";
 
-const USER_LS = "currentUser",
-  SHOWING_CN = "showing";
-
-function saveName(text) {
-  localStorage.setItem(USER_LS, text);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  const currentValue = input.value;
-  paintGreeting(currentValue);
-  saveName(currentValue);
-}
-
-function paintGreeting(text) {
-  form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
-}
-
-function askForName() {
-  form.classList.add(SHOWING_CN);
-  form.addEventListener("submit", handleSubmit);
-}
-
-function loadName() {
+function loadName(){
   const currentUser = localStorage.getItem(USER_LS);
-  if (currentUser === null) {
-    askForName();
-  } else {
-    paintGreeting(currentUser);
+  if(currentUser == null){
+    
+    }
   }
-}
 
-function init() {
+function init(){
   loadName();
 }
 
 init();
+// const USER_LS = "currentUser",
+  // SHOWING_CN = "showing";
+
+// function saveName(text) {
+//   localStorage.setItem(USER_LS, text);
+// }
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const currentValue = input.value;
+//   paintGreeting(currentValue);
+//   saveName(currentValue);
+// }
+
+// function paintGreeting(text) {
+//   form.classList.remove(SHOWING_CN);
+//   greeting.classList.add(SHOWING_CN);
+//   greeting.innerText = `Hello ${text}`;
+// }
+
+// function askForName() {
+//   form.classList.add(SHOWING_CN);
+//   form.addEventListener("submit", handleSubmit);
+// }
+
+// function loadName() {
+//   const currentUser = localStorage.getItem(USER_LS);
+//   if (currentUser === null) {
+//     askForName();
+//   } else {
+//     paintGreeting(currentUser);
+//   }
+// }
+
+// function init() {
+//   loadName();
+// }
+
+// init();
